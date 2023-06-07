e0da9acd3c821237546de3b9239f27377c6da033
scivieAPC <135734394+scivieAPC@users.noreply.github.com>
2023-06-07 08:09:09 -0400

    Update streamlit_app.py
    Snowflake diner menu update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
 
-streamlit.title('Hello World!')
+streamlit.title('My Parents New Healthy Diner')
 
+streamlit.header('Breakfasst menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Bland Bran')
