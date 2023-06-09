b385aea7d3837a500e0caadefa9313d884e36560
scivieAPC <135734394+scivieAPC@users.noreply.github.com>
2023-06-09 13:04:58 -0400

    Update streamlit_app.py
    Queried the fruit load list

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,6 @@ import requests
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -38,3 +33,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/fruit_choic
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+my_data_row = my_cur.fetchone()
+streamlit.text("The Fruit Load List Conatins")
+streamlit.text(my_data_row)
