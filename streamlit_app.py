a0ff7c7fa55545053bc531547f1ddc7443fab09f
scivieAPC <135734394+scivieAPC@users.noreply.github.com>
2023-06-09 13:30:00 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * pc_rivery_db.public.FROM FRUIT_LOAD_LIST")
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("The Fruit Load List Conatins")
 streamlit.text(my_data_row)
